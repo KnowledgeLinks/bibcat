98a0a22bdb09047c026a3cce4006701b816c14d2
Jeremy Nelson <jermnelson.gmail.com>
2015-07-01 17:12:41 -0600

    Continued to work on project

--- badges.py
+++ badges.py
@@ -14,6 +14,8 @@ __version_info__ = ('0', '0', '3')
 __version__ = '.'.join(__version_info__)
 
 import argparse
+import configparser
+import datetime
 import falcon
 import hashlib
 import json
@@ -24,6 +26,7 @@ import re
 import redis
 import requests
 import socket
+import time
 import urllib.request
 import uuid
 import lib.semantic_server.app  as semantic_server
@@ -39,7 +42,7 @@ from string import Template
 OB = rdflib.Namespace('http://schema.openbadges.org/')
 
 PREFIX = """PREFIX bf: <{}>
-fedora: <{}>
+PREFIX fedora: <{}>
 PREFIX iana: <{}>
 PREFIX ob: <{}> 
 PREFIX rdf: <{}>
@@ -55,6 +58,9 @@ PREFIX xsd: <{}>""".format(BF,
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 CURRENT_DIR = os.path.dirname(PROJECT_ROOT)
 TRIPLESTORE_URL = "http://localhost:8081/bigdata/sparql"
+
+CONFIG = configparser.ConfigParser()
+CONFIG.read(os.path.abspath(os.path.join(PROJECT_ROOT, "application.cfg")))
 
 FIND_ASSERTION_SPARQL = """{}
 SELECT DISTINCT *
@@ -94,10 +100,16 @@ WHERE {{{{
 FIND_IMAGE_SPARQL = """{}
 SELECT DISTINCT ?image
 WHERE {{{{
-  ?subject fedora:uuid <{{}}> .
+  ?subject schema:alternativeName "{{}}"^^xsd:string  .
   ?subject iana:describes ?image .
 }}}}""".format(PREFIX)
 
+FIND_KEYWORDS_SPARQL = """{}
+SELECT ?keyword
+WHERE {{{{
+   ?subject schema:alternativeName "{{}}"^^xsd:string .
+   ?subject schema:keywords ?keyword .
+}}}}""".format(PREFIX)
 
 def default_graph():
     graph = rdflib.Graph()
@@ -105,6 +117,7 @@ def default_graph():
     graph.namespace_manager.bind('ob', OB)
     graph.namespace_manager.bind('rdf', RDF)
     graph.namespace_manager.bind('schema', SCHEMA)
+    graph.namespace_manager.bind('owl', OWL)
     return graph
 
 
@@ -221,45 +234,51 @@ def issue_badge(email, event):
     """
     if email is None or event is None:
         raise ValueError("email and event cannot be None")
-    with badge_app.app_context():
-        badge_url = current_app.url_for(
-            'badge_assertion',
-            event=event,
-            uuid=str(badge_graph.value(
-                subject=rdflib.URIRef(fedora_url),
-                predicate=rdflib.URIRef(
-                    'http://fedora.info/definitions/v4/repository#uuid'))))
     badge_assertion_graph = default_graph()
-    badge_uri = rdflib.BNode()
+    badge_uri = rdflib.URIRef('http://intro2libsys.info/badge/temp')
     badge_assertion_graph.add((badge_uri,
                                OB.verify,
                                OB.hosted))
-    identity_hash = hashlib.sha256(email)
-    identity_hash.update(badge_app.config['IDENTITY_SALT'])
+    identity_hash = hashlib.sha256(email.encode())
+    identity_hash.update(CONFIG.get('BADGE', 'identity_salt').encode())
     badge_assertion_graph.add(
         (badge_uri, 
          OB.identity,
          rdflib.Literal("sha256${0}".format(identity_hash.hexdigest()))))
+    badge_assertion_graph.add(
+        (badge_uri,
+         OB.verify,
+         OB.hosted))
+    badge_assertion_graph.add(
+        (badge_uri,
+         OB.issuedOn,
+         rdflib.Literal(datetime.datetime.utcnow().isoformat())))    
+    new_badge = Resource(config=config)
+    badge_uri = new_badge.__create__(
+        rdf=badge_assertion_graph,
+        binary=b'a')
 
-    raw_data = """
-INSERT DATA {
-  <> schema:email "$email" .
-  <> openbadge:badge  <$badge_class_uri> .
-  <> openbadge:identity "$sha256" .
-  <> openbadge:verify openbadge:hosted .
-  <> openbadge:issuedOn "$issuedOn"
-}"""
+#
+#    raw_data = """
+#INSERT DATA {
+#  <> schema:email "$email" .
+#  <> ob:badge  <$badge_class_uri> .
+#  <> ob:identity "$sha256" .
+#  <> ob:verify openbadge:hosted .
+#  <> ob:issuedOn "$issuedOn"
+#}"""
 
-    update_request = urllib.request.Request(
-        fedora_url,
-        method='PATCH',
-        data=sparql.encode(),
-        headers={"Content-Type": "application/sparql-update"})
-    result = urllib.request.urlopen(update_request)
-    repository.insert("/".join([repository.base_url, 'rest', 'badges']),
-        "schema:AchieveAction",
-        fedora_url)
+#    update_request = urllib.request.Request(
+#        fedora_url,
+#        method='PATCH',
+#        data=sparql.encode(),
+#        headers={"Content-Type": "application/sparql-update"})
+#    result = urllib.request.urlopen(update_request)
+#    repository.insert("/".join([repository.base_url, 'rest', 'badges']),
+#        "schema:AchieveAction",
+#        fedora_url)
     #bake_badge(badge_uri)
+    print(badge_uri)
     return str(badge_uri)
 
 
@@ -282,43 +301,128 @@ class BadgeCollection(object):
 
 class Badge(object):
 
-    def __init__(self, ):
-        self.uuid = None
-
-    def on_get(self, req, resp, uuid=None):
-        if uuid:
-            self.uuid = uuid
-       
+    def on_get(self, req, resp, name, uuid):
+        result = requests.post(TRIPLESTORE_URL,
+            data={"query": FIND_ASSERTION_SPARQL.format(uid),
+                  "format": 'json'})
+        if result.status_code > 399:
+            raise falcon.HTTPInternalServerError(
+                "Cannot retrieve {}/{} badge".format(name, uuid),
+                result.text)
+        badge_base_url = CONFIG.get('BADGE', 'badge_base_url')
+        badge = {
+            "uid": uid,
+            "recipient": bindings[0]['recipient']['value'],
+            "badge": "{}/badges/{}".format(badge_base_url, name),
+            "image": "{}/badges/{}.png".format(badge_base_url, uuid),
+            "issuedOn": int(time.mktime(issuedOn.timetuple())),
+            "verify": {
+                "type": "hosted",
+                "url": "{}/badges/{}.json".format(
+                            badge_base_url,
+                            name)
+            }
+        }
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps(badge)
 
 class BadgeClass(object):
 
     def __init__(self):
         pass
 
-    def on_get(self, req, resp, name):
-        resp.status = falcon.HTTP_200
-        resp.body = json.dumps(output)
+
+    def __keywords__(self, name):
+        sparql = FIND_KEYWORDS_SPARQL.format(name)
         result = requests.post(
             TRIPLESTORE_URL,
-            data={"query": FIND_CLASS_SPARQL.format(name)})
+            data={"query": sparql,
+                  "format": "json"})
+        info = result.json()['results']['bindings']
+        output = [] 
+        for result in info:
+            output.append(result.get('keyword').get('value'))
+        return list(set(output))
 
-
-    def __on_get__(self):
+    def on_get(self, req, resp, name):
+        resp.status = falcon.HTTP_200
+        sparql = FIND_CLASS_SPARQL.format(name)
+        result = requests.post(
+            TRIPLESTORE_URL,
+            data={"query": sparql,
+                  "format": "json"})
+        if result.status_code > 399:
+            raise falcon.HTTPInternalServerError(
+                   "Cannot retrieve {} Badge Class".format(name),
+                   result.text)
+        info = result.json()['results']['bindings'][0]
+        keywords = self.__keywords__(name)
+        badge_base_url = CONFIG.get('BADGE', 'badge_base_url')
         badge_class_json = {
-            "name": self.cache.hget(name_hash, 'source'),
-            "description": self.cache.hget(desc_hash, "source")
-           # "critera": url_for('badge_criteria', badge=badge_classname),
-           # "image": url_for('badge_image', badge=badge_classname),
-           # "issuer": url_for('badge_issuer_organization'),
-           # "tags": keywords
+            "name": info.get('name').get('value'),
+            "description": info.get('description').get('value'),
+            "critera": '{}/badges/{}/criteria'.format(
+                           badge_base_url,
+                           name),
+            "image": '{}/badges/{}.png'.format(
+                          badge_base_url,
+                          name),
+            "issuer": '{}/badges/issuer'.format(badge_base_url),
+            "tags": keywords
         }
         resp.body = json.dumps(badge_class_json)
-         
+
+
+class BadgeClassCriteria(object):
+
+    def on_get(self, req, resp, name):
+        sparql = FIND_CRITERIA_SPARQL.format(name)
+        badge_result = requests.post(
+            TRIPLESTORE_URL,
+            data={"query": sparql,
+                  "format": "json"})
+        if badge_result.status_code > 399:
+
+            raise falcon.HTTPInternalServerError(
+                "Cannot retrieve {}'s criteria".format(name),
+                badge_result.text)
+        bindings = badge_result.json().get('results').get('bindings')
+        if len(bindings) < 1:
+            raise falcon.HTTPNotFound()
+        name ="Criteria for {} Open Badge".format(bindings[0]['name']['value']) 
+        badge_criteria = {
+            "name": name,
+            "educationalUse": list(set([row.get('criteria').get('value') for row in bindings]))
+        }
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps(badge_criteria) 
         
+
+class BadgeImage(object):
+
+    def on_get(self, req, resp, name):
+        resp.content_type = 'image/png'
+        sparql = FIND_CLASS_IMAGE_SPARQL.format(name)
+        img_exists = requests.post(
+            TRIPLESTORE_URL,
+            data={"query": sparql,
+                  "format": "json"})
+        if img_exists.status_code > 399:
+            raise falcon.HTTPInternalServerError(
+                "Cannot retrieve {}'s image".format(name),
+                img_exists.text)
+        img_url = img_exists.json()['results']['bindings'][0]
+        img_url = img_url.get('image').get('value')
+        img_result = requests.get(img_url)
+        resp.body = img_result.content
 
 #semantic_server.api.add_route("badge/{uuid}", Badge())
 semantic_server.api.add_route("/badges", BadgeClass())
 semantic_server.api.add_route("/badges/{name}", BadgeClass())
+semantic_server.api.add_route("/badges/{name}.json", BadgeClass())
+semantic_server.api.add_route("/badges/{name}.png", BadgeImage())
+semantic_server.api.add_route("/badges/{name}/criteria", BadgeClassCriteria())
+semantic_server.api.add_route("/badges/{name}/{uuid}", Badge())
 
 class Services(object):
 
@@ -410,6 +514,7 @@ def main(args):
             debug=True)
         semantic_server.main()
     elif args.action.startswith('start'):
+        print("Starting REST API on port 18150")
         semantic_server.main()
     elif args.action.startswith('issue'):
         email = args.email
