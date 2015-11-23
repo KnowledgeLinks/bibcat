9804b04340b741c65892b7ecc824868fd82a613b
Jeremy Nelson <jermnelson@gmail.com>
2015-11-23 12:42:29 -0700

    Added Flask Open Badges blueprint

---
+++ badges/blueprint.py
@@ -0,0 +1,121 @@
+__author__ = "Jeremy Nelson"
+
+
+import requests
+from flask import abort, Blueprint, jsonify, render_template, Response
+from flask_negotiate import produces
+
+
+open_badge = Blueprint("open_badge", __name__,
+                       template_folder="templates")
+
+@open_badge.route("/Assertion/<event>/<uid>")
+@open_badge.route("/Assertion/<event>/<uid>.json")
+@produces('application/json')
+def badge_assertion(event, uid):
+    """Route returns individual badge assertation json or 404 error if not
+    found in the repository.
+
+    Args:
+        event: Badge Type (Camp, Projects, Institutions, etc.)
+        uid: Unique ID of the Badge
+
+    Returns:
+        Assertion JSON of issued badge
+    """
+    badge_uri = repository.sparql(uuid_template(uuid=uid))
+    if badge_uri is None:
+        abort(404)
+    badge_graph = rdflib.Graph().parse(badge_uri)
+    badge = {
+
+
+    }
+    return jsonify(badge)
+
+@open_badge.route("/BadgeClass/<badge_classname>")
+@open_badge.route("/BadgeClass/<badge_classname>.json")
+@produces('application/json', 'application/rdf+xml', 'text/html')
+def badge_class(badge_classname):
+    """Route generates a JSON BadgeClass
+    <https://github.com/mozilla/openbadges-specification/> for each Islandora
+    badge.
+
+    Args:
+        badge_classname: Name of Badge (Camp, Projects, Institutions, etc.)
+
+    Returns:
+        Badge Class JSON
+    """
+    if not badge_classname in badges:
+        abort(404)
+    event = badges.get(badge_classname)
+    badge_rdf = event.get('graph')
+    badge_class_uri = event.get('uri')
+    keywords = [str(obj) for obj in badge_rdf.objects(
+        subject=badge_class_uri,
+        predicate=schema_namespace.keywords)]
+    badge_class_json = {
+        "name": badge_rdf.value(
+            subject=badge_class_uri,
+            predicate=schema_namespace.name),
+        "description": badge_rdf.value(
+            subject=badge_class_uri,
+            predicate=schema_namespace.description),
+        "critera": url_for('badge_criteria', badge=badge_classname),
+        "image": url_for('badge_image', badge=badge_classname),
+        "issuer": url_for('badge_issuer_organization'),
+        "tags": keywords
+        }
+    return jsonify(badge_class_json)
+
+@badge_app.route("/Criteria/<badge>")
+@produces('application/json')
+def badge_criteria(badge):
+    """Route displays the criteria for the badge class
+
+    Args:
+        badge: Name of Badge (Camp, Projects, Institutions, etc.)
+
+    Returns:
+        JSON of badge's critera
+    """
+    event = badges.get(badge)
+    badge_rdf = event.get('graph')
+    badge_class_uri = event.get('uri')
+    badge_criteria = {
+        "name": "Criteria for {}".format(
+            badge_rdf.value(
+                subject=badge_class_uri,
+                predicate=schema_namespace.name)),
+        "educationalUse": [str(obj) for obj in badge_rdf.objects(
+            subject=badge_class_uri, predicate=schema_namespace.educationalUse)]
+    }
+    return jsonify(badge_criteria)
+
+
+@open_badge.route("/Issuer")
+def badge_issuer_organization():
+    "Route generates JSON for the badge issuer's organization"
+    organization = {
+        "name": badge_app.config['BADGE_ISSUER_NAME'],
+        "url": badge_app.config['BADGE_ISSUER_URL']
+    }
+    return jsonify(organization)
+
+
+@open_badge.route("/<badge>.png")
+@open_badge.route("/<badge>/<uid>.png")
+def badge_image(badge, uid=None):
+    if uid is not None:
+        img_url = repository.sparql(uuid_template(uuid=uid))
+    else:
+        if not badge in badges:
+            abort(404)
+        img_url = '/'.join(str(badges[badge]['url']).split("/")[:-1])
+    img_response = requests.get(img_url)
+    if img_response.status_code > 399:
+        abort(500)
+    return Response(img_response.text, mimetype='image/png')
+
+
