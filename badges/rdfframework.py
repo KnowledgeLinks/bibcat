56c55149562af8f08bbf42ec647ad589e9bda16e
Jeremy Nelson <jermnelson.gmail.com>
2015-12-22 15:47:12 -0700

    Completed unit tests for rdf_datatype class, started Rdf class unit tests

--- badges/rdfframework.py
+++ badges/rdfframework.py
@@ -1,7 +1,13 @@
 from flask import current_app, json
 from .utilities import render_without_request
 import requests
-#from rdflib import RDF, RDFS, OWL, XSD, FOAF, SKOS, DOAP, DC, DCTERMS, VOID
+from rdflib import Namespace, RDF, RDFS, OWL, XSD #! Not sure what VOID is
+
+DC = Namespace("http://purl.org/dc/elements/1.1/")
+DCTERMS = Namespace("http://purl.org/dc/terms/") 
+DOAP = Namespace("http://usefulinc.com/ns/doap#")
+FOAF = Namespace("http://xmlns.com/foaf/spec/")
+SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
 
 class rdf_framework(object):
     '''base class for Knowledge Links' Graph database RDF vocabulary framework'''
@@ -35,12 +41,13 @@ class rdf_framework(object):
             current_app.config.get('TRIPLESTORE_URL'),
             data={"query": sparql,
                   "format": "json"})
-        print("***** Querying tipplestore ****")
         return json.loads(classList.json().get('results').get('bindings')[0]['appClasses']['value'])
-        
+
+#! Standard Python practice is to use CamelCase for Class names
+#! so rdf_class could be RDFClass, RdfClass, or rdfClass
 class rdf_class(object):
     '''RDF Class for a RDF Class object. 
-       Uesed for manipulating and validating an RDF Class subject'''
+       Used for manipulating and validating an RDF Class subject'''
        
     def __init__(self,jsonObj):
         for p in jsonObj:
@@ -59,45 +66,51 @@ class rdf_class(object):
         
     def validatePrimaryKey(self, dataValue):
         '''query to see if PrimaryKey is Valid'''
-        returnVal = False
+        returnVal = False #! This variable is never used
         try:
             pkey = self.primaryKey
             dataType = self.properties[self.findPropName(pkey)]['storageType']
             if dataType == 'literal':
                 dataType = self.properties[self.findPropName(pkey)].get('range',dataType)
-            return self.__makeTriple("?uri","a",iri(self.classUri)) + self.__makeTriple("?uri",iri(pkey),rdf_datatype(dataType).sparql(dataValue))
+            return self.__makeTriple(
+                    "?uri",
+                     "a",
+                     iri(self.classUri)) + self.__makeTriple("?uri", iri(pkey), rdf_datatype(dataType).sparql(dataValue))
         except:
             pass
         else:
             return "no primaryKey"
         
     def __makeTriple (self,s,p,o):
-        "takes a subject predicate and object and joins them with a space in between"
-        return "{} {} {} .".format(s, p, o)
+        "takes a subject predicate and object and joins them with a sp ace in between"
+        return "{s} {p} {o} .".format(s=s, p=p, o=0)
         
     def findPropName (self,propUri):
         "cycle through the class properties object to find the property name"
         #print(self.properties)
         for p in self.properties:
-            print("p--",p," -- ",self.properties[p]['propUri'])
+            #print("p--",p," -- ",self.properties[p]['propUri'])
             if self.properties[p]['propUri'] == propUri:
-                print ('propName is ',p)
+                #print ('propName is ',p)
                 return p
                 
     def formatDataType(self,dataValue,dataType):
         "formats a dataValue and dataType for SPARQL query"
         if dataType == "literal":
-            return '"' + dataValue + '"'
+            return '"{}"'.format(dataValue)
         elif dataType == "object":
             return self.iri(dataValue)
         else:
             return dataValue
-    
+
+#! Standard Python practice is to use CamelCase for Class names
+#! so rdf_datatype could be RDFDataType, RdfDataType, or rdfDataType
 class rdf_datatype(object):
     "This class will generate a rdf data type"
+
     def __init__(self, rdfDataType):
-        self.__dataTypes(rdfDataType)
-        
+        self.__dataTypes__(rdfDataType)
+
     def sparql(self,dataValue):
         "formats a value for a sparql triple"
         if self.name == "object":
@@ -105,28 +118,33 @@ class rdf_datatype(object):
         elif self.name == "literal":
             return '"{}"'.format(dataValue)
         elif self.name == "boolean":
-            return '"{}""^^{}'.format(str(dataValue).lower(),
+            return '"{}"^^{}'.format(str(dataValue).lower(),
                                       self.prefix)
         else:
             return '"{}"^^{}'.format(dataValue, self.prefix)
-        
-    def __dataTypes(self,lookup):
+    
+    #! Why isn't this function in __init__, will it ever be used outside of
+    #! __init__ call?   
+    def __dataTypes__(self,lookup):
         "sets the class attributes"
-        val = lookup.replace("http://www.w3.org/2001/XMLSchema#","").\
+        #! What happens if none of these replacements? 
+        val = lookup.replace(str(XSD),"").\
                 replace("xsd:","").\
                 replace("rdf:","").\
-                replace("http://www.w3.org/1999/02/22-rdf-syntax-ns#","")
+                replace(str(RDF),"")
         self.prefix = "xsd:{}".format(val)
-        self.iri = iri("http://www.w3.org/2001/XMLSchema#{}".format(val))
+        self.iri = iri("{}{}".format(str(XSD), val))
         self.name = val
         if val.lower() == "literal" or val.lower() == "langstring":
             self.prefix = "rdf:{}".format(val)
-            self.iri = iri("http://www.w3.org/1999/02/22-rdf-syntax-ns#" + val)
+            self.iri = iri(str(RDF) + val)
         elif val.lower() == "object":
             self.prefix = "objInject"
+            #! Why is uri a new property if an object?
             self.uri = "objInject"
         
         
+#! Should we test to see if uriString is a valid IRI?
 def iri(uriString):
     "converts a string to an IRI or returns an IRI if already formated"
     if uriString[:1] != "<":
--- tests/test_rdfframework.py
+++ tests/test_rdfframework.py
@@ -1,5 +1,6 @@
 __author__ = "Jeremy Nelson, Mike Stabile"
 
+import json
 import os
 import sys
 import unittest
@@ -25,15 +26,126 @@ class TestIri(unittest.TestCase):
 class TestRdfClass(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.person_json = json.loads("""{
+    "Person": {
+        "className": "Person",
+        "classUri": "https://schema.org/Person",
+        "primaryKey": "https://schema.org/email",
+        "storageType": "object",
+        "properties": {
+            "email": {
+                "propUri": "https://schema.org/email",
+                "range": "http://www.w3.org/2001/XMLSchema#string",
+                "storageType": "literal",
+        "required": true
+            },
+            "familyName": {
+                "propUri": "https://schema.org/familyName",
+                "range": "http://www.w3.org/2000/01/rdf-schema#literal",
+                "storageType": "literal",
+        "required": true
+            },
+            "givenName": {
+                "propUri": "https://schema.org/givenName",
+                "range": "http://www.w3.org/2001/XMLSchema#string",
+                "storageType": "literal",
+        "required": true
+            },
+     "image": {
+                "propUri": "https://schema.org/image",
+                "range": ["http://schema.org/ImageObject","http://schema.org/URL"],
+                "storageType": ["object","literal"],
+        "required": false
+            }
+        }
+    }
+}""")
+        
 
+    def test_init(self):
+        person = rdf_class(self.person_json)
+        self.assertEqual(
+            person.Person.get('classUri'), 
+            "https://schema.org/Person")
+        
 class TestRdfDatatype(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.langstring_instance = rdf_datatype("langstring")
+        self.literal_instance = rdf_datatype("literal")
+        self.obj_instance = rdf_datatype("object")
+        self.str_instance = rdf_datatype("http://www.w3.org/2001/XMLSchema#string")
 
     def test_init(self):
         instance = rdf_datatype("https://schema.org/Person")
         self.assertEqual(instance.name, "https://schema.org/Person")
-        self.assertEqual(instance.iri, "<https://schema.org/Person>")
+        self.assertEqual(
+            instance.iri, 
+            "<http://www.w3.org/2001/XMLSchema#https://schema.org/Person>")
+        #! Is this what we want as a prefix?
+        self.assertEqual(instance.prefix, "xsd:https://schema.org/Person")
+
+    def test_init_errors(self):
+        self.assertRaises(TypeError, rdf_datatype)
+
+    def test_literal_datatype(self):
+        self.assertEqual(self.literal_instance.name, "literal")
+        self.assertEqual(self.literal_instance.prefix, "rdf:literal")
+        self.assertEqual(
+            self.literal_instance.iri,
+            "<http://www.w3.org/1999/02/22-rdf-syntax-ns#literal>")
+
+
+    def test_langstring_datatype(self):
+        self.assertEqual(self.langstring_instance.prefix, "rdf:langstring")
+        self.assertEqual(
+            self.langstring_instance.iri,
+            "<http://www.w3.org/1999/02/22-rdf-syntax-ns#langstring>")
+
+
+    def test_object_datatype(self):
+        self.assertEqual(self.obj_instance.prefix, "objInject")
+        self.assertEqual(self.obj_instance.uri, "objInject")
+        self.assertEqual(self.obj_instance.iri, "<http://www.w3.org/2001/XMLSchema#object>")
+
+        
+    def test_str_datatype(self):
+        self.assertEqual(self.str_instance.prefix, "xsd:string")
+        self.assertEqual(
+            self.str_instance.iri,
+            "<http://www.w3.org/2001/XMLSchema#string>")
+
+    def test_sparql_object(self):
+        self.assertEqual(
+            self.obj_instance.sparql("http://knowledgelinks.io/example/1"),
+            "<http://knowledgelinks.io/example/1>")
+        #! Should raise an Error?
+        self.assertEqual(
+            self.obj_instance.sparql("Test String"),
+            "<Test String>")
+
+    def test_sparql_langstring(self):
+        self.assertEqual(
+            self.langstring_instance.sparql("eng"),
+            '"eng"^^rdf:langstring')
+
+
+    def test_sparql_literal(self):
+        self.assertEqual(
+            self.str_instance.sparql("Test String"),
+            '"Test String"^^xsd:string')
+
+    def test_sparql_boolean(self):
+        boolean_instance = rdf_datatype("boolean")
+        self.assertEqual(
+            boolean_instance.sparql(True),
+            '"true"^^xsd:boolean')
+
+            
+    def test_sparql(self):
+        badge_class_instance = rdf_datatype(str(DC.name))
+        self.assertEqual(
+            badge_class_instance.sparql("Test"),
+            '"Test"^^xsd:http://purl.org/dc/elements/1.1/name')
+
 
