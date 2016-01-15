5d70e5ded338d0fde2188988cc199829d26b14f7
Jeremy Nelson <jermnelson@gmail.com>
2016-01-15 16:37:41 -0700

    Refactoring blueprint to pylint standards

--- badges/blueprint.py
+++ badges/blueprint.py
@@ -1,3 +1,4 @@
+"""Flask Blueprint for Open Badges"""
 __author__ = "Jeremy Nelson, Mike Stabile"
 
 import json
@@ -6,64 +7,86 @@ import re
 from flask import abort, Blueprint, jsonify, render_template, Response, request
 from flask import redirect, url_for
 from flask_negotiate import produces
-from flask.ext.login import login_required
+from flask.ext.login import login_required, login_user
 
 from . import new_badge_class, issue_badge
 from .forms import NewBadgeClass, NewAssertion, rdf_form_factory
-from .graph import *
-from .utilities import render_without_request 
-from .rdfframework import *   
-    
+from .graph import FIND_ALL_CLASSES, FIND_IMAGE_SPARQL
+from .utilities import render_without_request
+from .rdfframework import rdf_framework_form_factory
+from .user import User
+
 open_badge = Blueprint("open_badge", __name__,
                        template_folder="templates")
 open_badge.config = {}
 
+
 @open_badge.record
 def record_params(setup_state):
+    """Function takes the setup_state and updates configuration from
+    the active application.
+
+    Args:
+        setup_state -- Setup state of the application.
+    """
     app = setup_state.app
-    open_badge.config = dict([(key, value) for (key,value) in app.config.items()])
+    open_badge.config = dict(
+        [(key, value) for (key, value) in app.config.items()]
+    )
     base_url = open_badge.config.get('ORGANIZATION').get('url')
     triplestore_url = open_badge.config.get('TRIPLESTORE_URL')
     # Strip off trailing forward slash for TTL template
     if base_url.endswith("/"):
         base_url = base_url[:-1]
     # if the extensions exist in the triplestore drop the graph
+    stmt = "DROP GRAPH <http://knowledgelinks.io/ns/application-framework/>;"
     drop_extensions = requests.post(
         url=triplestore_url,
-        params = {"update":"DROP GRAPH <http://knowledgelinks.io/ns/application-framework/>;"})
+        params={"update": stmt})
     # render the extensions with the base URL
-    # must use a ***NON FLASK*** routing since flask is not completely initiated 
-    rdf_resource_templates = ["kds-app.ttl","kds-vocab.ttl","kds-resources.ttl"]
+    # must use a ***NON FLASK*** routing since flask is not completely initiated
+    rdf_resource_templates = [
+        "kds-app.ttl",
+        "kds-vocab.ttl",
+        "kds-resources.ttl"]
     rdf_data = []
     for template in rdf_resource_templates:
-        rdf_data.append(render_without_request(
-        template,
-        base_url=base_url))
-    # load the extensions in the triplestore 
+        rdf_data.append(
+            render_without_request(
+                template,
+                base_url=base_url))
+    # load the extensions in the triplestore
+    context_uri = "http://knowledgelinks.io/ns/application-framework/"
     for data in rdf_data:
         result = requests.post(
             url=triplestore_url,
-            headers = {"Content-Type":"text/turtle"},
-            params = {"context-uri":"http://knowledgelinks.io/ns/application-framework/"},
-            data = data)
+            headers={"Content-Type": "text/turtle"},
+            params={"context-uri": context_uri},
+            data=data)
+        if result.status_code > 399:
+            raise ValueError("Cannot load extensions in {}".format(
+                triplestore_url))
     #get_framework()
- 
+
 def get_badge_classes():
+    """Helper function retrieves all badge classes from the triplestore"""
     all_badges_response = requests.post(
-       open_badge.config.get('TRIPLESTORE_URL'),
-       data={"query": FIND_ALL_CLASSES,
-             "format": "json"})
+        open_badge.config.get('TRIPLESTORE_URL'),
+        data={"query": FIND_ALL_CLASSES,
+              "format": "json"})
     if all_badges_response.status_code > 399:
         abort(502)
     bindings = all_badges_response.json().get('results').get('bindings')
     #uid = re.sub(r'^(.*[#/])','',bindings[0].get('subject')['value'])
     #, re.sub(r'^(.*[#/])','',r.get('subject')['value'])
-    return [(r.get('altName')['value'], r.get('name')['value']) for r in bindings]
+    return [(r.get('altName')['value'],
+             r.get('name')['value']) for r in bindings]
 
 
 @open_badge.route("/Assertion/", methods=["POST", "GET"])
 #@login_required
 def add_badge_assertion():
+    """View for adding/editing assertion"""
     assertion_form = NewAssertion()
     assertion_form.badge.choices = get_badge_classes()
     if request.method.startswith("POST"):
@@ -82,7 +105,7 @@ def add_badge_assertion():
     print(assertion_form.__dict__)
     return render_template(
         "assertion.html",
-         form=assertion_form)
+        form=assertion_form)
 
 @open_badge.route("/Assertion/<uuid>")
 @open_badge.route("/Assertion/<uuid>.json")
@@ -100,19 +123,24 @@ def badge_assertion(uuid):
     """
     sparql = render_template(
         "jsonObjectQueryTemplate.rq",
-        uri_sparql_select = """
-            BIND ("{}" AS ?uid) .
-            BIND (URI(CONCAT("http://localhost:8080/fedora/rest/",SUBSTR(?uid, 1,2),"/",SUBSTR(?uid, 3,2),
-            "/",SUBSTR(?uid, 5,2),"/",SUBSTR(?uid, 7,2),"/",?uid)) AS ?uri) .""".format(uuid),
-        object_type = "Assertion")
-    assertion_response = requests.post( 
+        uri_sparql_select="""
+BIND ("{}" AS ?uid) .
+BIND (URI(CONCAT("http://localhost:8080/fedora/rest/",SUBSTR(?uid, 1,2),"/",SUBSTR(?uid, 3,2),
+"/",SUBSTR(?uid, 5,2),"/",SUBSTR(?uid, 7,2),"/",?uid)) AS ?uri) .""".format(
+                uuid),
+        object_type="Assertion")
+    assertion_response = requests.post(
         open_badge.config.get('TRIPLESTORE_URL'),
         data={"query": sparql,
               "format": "json"})
     if assertion_response.status_code > 399:
         abort(505)
-    raw_text = assertion_response.json().get('results').get('bindings')[0]['jsonString']['value']
-    return json.dumps(json.loads(raw_text),indent=4, sort_keys=True)
+    raw_text = assertion_response.json().get('results') \
+               .get('bindings')[0]['jsonString']['value']
+    return json.dumps(
+        json.loads(raw_text),
+        indent=4,
+        sort_keys=True)
 
 
 @open_badge.route("/BadgeClass/", methods=["POST", "GET"])
@@ -123,7 +151,7 @@ def add_badge_class():
     existing_badges = get_badge_classes()
     if request.method.startswith("POST"):
         raw_data = badge_class_form.image_file.data.read()
-        badge_url, badge_slug = new_badge_class(
+        _, badge_slug = new_badge_class(
             name=badge_class_form.name.data,
             description=badge_class_form.description.data,
             image=raw_data,
@@ -142,17 +170,17 @@ def add_badge_class():
 def add_user_class():
     """Displays Form for adding a user Form"""
     user_form = rdf_form_factory(
-        "NewUserForm", 
+        "NewUserForm",
         "obi:UserClass")
     return render_template(
         "user_class.html",
-        form=user_form()) 
+        form=user_form())
 
 @open_badge.route("/BadgeClass/<badge_classname>")
 @open_badge.route("/BadgeClass/<badge_classname>.json")
 @produces('application/json', 'application/rdf+xml', 'text/html')
 def badge_class(badge_classname):
-    """Route generates a JSON BadgeClass 
+    """Route generates a JSON BadgeClass
     <https://github.com/mozilla/openbadges-specification/> for each Islandora
     badge.
 
@@ -162,18 +190,23 @@ def badge_class(badge_classname):
     Returns:
         Badge Class JSON
     """
+    uri_sparql_select = """?uri schema:alternativeName "{}"^^xsd:string ."""\
+                        .format(badge_classname)
     sparql = render_template(
         "jsonObjectQueryTemplate.rq",
-        uri_sparql_select = """?uri schema:alternativeName "{}"^^xsd:string .""".format(badge_classname),
-        object_type = "BadgeClass") 
-    badge_class_response = requests.post( 
+        uri_sparql_select=uri_sparql_select,
+        object_type="BadgeClass")
+    badge_class_response = requests.post(
         open_badge.config.get('TRIPLESTORE_URL'),
         data={"query": sparql,
               "format": "json"})
     if badge_class_response.status_code > 399:
         abort(505)
-    raw_text = badge_class_response.json().get('results').get('bindings')[0]['jsonString']['value']
-    return "<pre>" + json.dumps(json.loads(raw_text),indent=4, sort_keys=True) +"</pre>"
+    raw_text = badge_class_response.json()\
+               .get('results')\
+               .get('bindings')[0]['jsonString']['value']
+    return "<pre>{}</pre>".format(
+        json.dumps(json.loads(raw_text), indent=4, sort_keys=True))
 
 @open_badge.route("/Criteria/<badge>")
 @produces('application/json')
@@ -189,7 +222,7 @@ def badge_criteria(badge):
     event = badges.get(badge)
     badge_rdf = event.get('graph')
     badge_class_uri = event.get('uri')
-    badge_criteria = {
+    badge_criteria_json = {
         "name": "Criteria for {}".format(
             badge_rdf.value(
                 subject=badge_class_uri,
@@ -197,7 +230,7 @@ def badge_criteria(badge):
         "educationalUse": [str(obj) for obj in badge_rdf.objects(
             subject=badge_class_uri, predicate=schema_namespace.educationalUse)]
     }
-    return jsonify(badge_criteria)
+    return jsonify(badge_criteria_json)
 
 
 @open_badge.route("/Issuer")
@@ -214,10 +247,11 @@ def badge_issuer_organization():
 @open_badge.route("/AssertionImage/<uid>.png")
 def badge_image(badge=None, uid=None):
     if uid is not None:
-        assertion_url = "http://localhost:8080/fedora/rest/{0}/{1}/{2}/{3}/{4}".format(
-            uid[0:2], uid[2:4], uid[4:6], uid[6:8], uid)
+        assertion_url = "http://localhost:8080/fedora/rest/{0}/{1}/{2}/{3}/{4}"\
+                       .format(
+                           uid[0:2], uid[2:4], uid[4:6], uid[6:8], uid)
         assertion_img_response = requests.post(
-         open_badge.config.get('TRIPLESTORE_URL'),
+            open_badge.config.get('TRIPLESTORE_URL'),
             data={"query": FIND_IMAGE_SPARQL.format(assertion_url),
                   "format": "json"})
         if assertion_img_response.status_code > 399:
@@ -236,63 +270,111 @@ def badge_image(badge=None, uid=None):
         abort(500)
     return Response(img_response.text, mimetype='image/png')
 
-@open_badge.route("/user/<formInstance>.html", methods=["POST", "GET"])
-def user_rdf_class(formInstance):
-    f = rdf_framework_form_factory("UserForm",'http://knowledgelinks.io/ns/data-resources/'+formInstance)
-    val = None
-    if request.method == "POST":
-        form = f(request.form)
+@open_badge.route("/login", methods=["GET", "POST"])
+def login_user_view():
+    """Login view for badges"""
+    login_form = rdf_framework_form_factory(
+        "LoginForm",
+        'http://knowledgelinks.io/ns/data-resources/new')
+    if request.method.startswith("POST"):
+        form = login_form(request.form)
         val = form.validate()
+        #username = request.form.get("userName")
+        #pwd = request.form.get("password")
+        login_user(User())
     else:
-        form = f()     
+        form = login_form()
     return render_template(
         "app_form_template.html",
-        actionUrl="http://localhost:20151/badges/user/"+formInstance+".html",
+        actionURL=url_for("login_user_view"),
         form=form,
-        jsonFields=json.dumps(form.rdfFieldList,indent=4),
+        jsonFields=json.dumps(
+            form.rdfFieldList,
+            indent=4,
+            validated=val))
+
+@open_badge.route("/user/<form_instance>.html", methods=["POST", "GET"])
+def user_rdf_class(form_instance):
+    """View for user class
+
+    Args:
+        form_instance:   Type of form (new, edit)
+    """
+    form_class = rdf_framework_form_factory(
+        "UserForm",
+        'http://knowledgelinks.io/ns/data-resources/'+form_instance)
+    val = None
+    if request.method == "POST":
+        form = form_class(request.form)
+        val = form.validate()
+    else:
+        form = form_class()
+    return render_template(
+        "app_form_template.html",
+        actionUrl=url_for("user_rdf_class", form_instance=form_instance),
+        form=form,
+        jsonFields=json.dumps(form.rdfFieldList, indent=4),
         validated=val)
 
-@open_badge.route("/badgeTestForm/<formInstance>.html", methods=["POST", "GET"])
-def badge_rdf_class(formInstance):
-    f = rdf_framework_form_factory("BadgeForm",'http://knowledgelinks.io/ns/data-resources/'+formInstance)
+@open_badge.route("/badgeTestForm/<form_instance>.html", methods=["POST", "GET"])
+def badge_rdf_class(form_instance):
+    """View for displaying a badge Test form
+
+    Args:
+        form_instance -- Type of form (new, edit)
+    """
+    form_class = rdf_framework_form_factory(
+        "BadgeForm",
+        'http://knowledgelinks.io/ns/data-resources/'+form_instance)
     val = None
     if request.method == "POST":
-        form = f(request.form)
+        form = form_class(request.form)
         val = form.validate()
     else:
-        form = f()     
+        form = form_class()
     return render_template(
         "app_form_template.html",
-        actionUrl="http://localhost:20151/badges/badgeTestForm/"+formInstance+".html",
+        actionUrl=url_for("badge_rdf_class", form_instance=form_instance),
         form=form,
-        jsonFields=json.dumps(form.rdfFieldList,indent=4),
+        jsonFields=json.dumps(form.rdfFieldList, indent=4),
         validated=val)
-        
-@open_badge.route("/assertionTestForm/<formInstance>.html", methods=["POST", "GET"])
-def assertion_rdf_class(formInstance):
-    f = rdf_framework_form_factory("AssertionForm",'http://knowledgelinks.io/ns/data-resources/'+formInstance)
+
+@open_badge.route("/assertionTestForm/<form_instance>.html",
+                  methods=["POST", "GET"])
+def assertion_rdf_class(form_instance):
+    """View displays for Assertion test form
+
+    Args:
+        form_instance -- Type of form (new, edit)
+    """
+    form_class = rdf_framework_form_factory(
+        "AssertionForm",
+        'http://knowledgelinks.io/ns/data-resources/'+form_instance)
     val = None
     if request.method == "POST":
-        form = f(request.form)
+        form = form_class(request.form)
         val = form.validate()
     else:
-        form = f()  
-    form = loadFormSelectOptions(form)   
+        form = form_class()
+    form = loadFormSelectOptions(form)
     return render_template(
         "app_form_template.html",
-        actionUrl="http://localhost:20151/badges/assertionTestForm/"+formInstance+".html",
+        actionUrl=url_for("assertion_rdf_class", form_instance=form_instance),
         form=form,
-        jsonFields=json.dumps(form.rdfFieldList,indent=4),
+        jsonFields=json.dumps(form.rdfFieldList, indent=4),
         validated=val)
-        
-@open_badge.route("/test/", methods=["POST", "GET"]) 
+
+@open_badge.route("/test/", methods=["POST", "GET"])
 def test_rdf_class():
-    f = rdf_framework_form_factory("AssertionForm",'http://knowledgelinks.io/ns/data-resources/NewForm')
-    framework=get_framework()
-    x=y
-    return "<pre>" + json.dumps(result,indent=4) + "</pre>"
-    
-@open_badge.route("/formjson/", methods=["POST", "GET"]) 
+    """View for displaying a test RDF class"""
+    #form_class = rdf_framework_form_factory(
+    #    "AssertionForm",
+    #    'http://knowledgelinks.io/ns/data-resources/NewForm')
+    #framework=get_framework()
+    return "<pre>" + json.dumps({"message": "test rdf class"}) + "</pre>"
+
+@open_badge.route("/formjson/", methods=["POST", "GET"])
 def form_rdf_class():
-    f = get_framework().rdf_form_dict
-    return "<pre>" + json.dumps(f,indent=4) + "</pre>"
+    """View for displaying the form json"""
+    form_class = get_framework().rdf_form_dict
+    return "<pre>{}</pre>".format(json.dumps(form_class, indent=4))
---
+++ badges/user.py
@@ -0,0 +1,7 @@
+"""Basic User Class for Badge Application"""
+from flask.ext.login import UserMixin
+
+class User(UserMixin):
+
+    def __init__(self, kwargs):
+        pass
