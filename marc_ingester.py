35ec83533c4993d2729fb2ccb18adfda726760c4
Jeremy Nelson <jermnelson.gmail.com>
2016-05-29 19:08:09 -0600

    Working MARC21 to BIBFRAME graph

--- marc_ingester.py
+++ marc_ingester.py
@@ -26,21 +26,39 @@ PREFIX relators: <{}>""".format(
     rdflib.RDFS,
     RELATORS)
 
-DEDUP_WORK = PREFIX + """
-SELECT DISTINCT ?work
-WHERE {{
-    ?work bf:Title "{0}" .
-    ?work relators:aut ?author .
-    ?author rdfs:value "{1}"
-}}"""
-GET_ENTITY_MARC = PREFIX + """
-SELECT ?prop ?marc
-WHERE {{
-    ?prop rdfs:domain <{0}> .
-    ?prop kds:marc2bibframe ?marc
-}}
-ORDER BY ?marc"""
+#GET_ENTITY_MARC = PREFIX + """
+#SELECT ?prop ?marc
+#WHERE {{
+#    ?prop rdfs:domain <{0}> .
+#    ?prop kds:marc2bibframe ?marc
+#}}
+#ORDER BY ?marc"""
 
+
+GET_LINKED_CLASSES = PREFIX + """
+SELECT ?dest_prop ?dest_class ?linked_range
+WHERE {{
+   ?subj kds:destClassUri ?dest_class .
+   ?subj kds:destPropUri ?dest_prop .
+   ?subj kds:linkedRange ?linked_range .
+   ?subj kds:linkedClass <{0}> .
+}}"""
+
+GET_DIRECT_PROPS = PREFIX + """
+SELECT ?dest_prop ?marc
+WHERE {{
+    ?subj kds:destClassUri <{0}> .
+    ?subj kds:destPropUri ?dest_prop .
+    ?subj kds:srcPropUri ?marc .
+}}"""
+
+GET_MARC = PREFIX + """
+SELECT ?marc
+WHERE {{
+    ?subj kds:srcPropUri ?marc .
+    ?subj kds:destClassUri <{0}> .
+    ?subj kds:linkedClass <{1}> .
+}}"""
 
 MARC2BIBFRAME = None
 TRIPLESTORE_URL = "http://localhost:9999/blazegraph/sparql"
@@ -58,10 +76,6 @@ def deduplicate(graph):
               "format": "json"})
     if result.status_code > 399:
         return
-    
-    
-    
-
 
 def match_marc(record, pattern):
     """Takes a MARC21 and pattern extracted from the last element from a 
@@ -74,11 +88,16 @@ def match_marc(record, pattern):
         list of subfield values
     """
     output = []
-    field_name = pattern[0:4]
+    field_name = pattern[1:4]
     indicators = pattern[4:6]
     subfield = pattern[-1]
     fields = record.get_fields(field_name)
     for field in fields:
+        if field.is_control_field():
+            start, end = pattern[4:].split("-")
+            output.append(field.data[int(start):int(end)+1])
+
+            continue
         indicator_key = "{}{}".format(
             field.indicators[0].replace(" ", "_"),
             field.indicators[1].replace(" ", "_"))
@@ -103,17 +122,19 @@ def process(filepath):
     marc_reader = pymarc.MARCReader(open(filepath, "rb"), 
         to_unicode=True)
     start = datetime.datetime.utcnow()
+    total = 0
     print("Started at {}".format(start))
     for i, record in enumerate(marc_reader):
         bf_graph = transform(record)
-        deduplicate(bf_graph)
+        #deduplicate(bf_graph)
         if not i%10 and i > 0:
             print(".", end="")
         if not i%100:
             print(i, end="")
+        total = i
     end = datetime.datetime.utcnow()
     print("\nFinished {} at {}, total time={} mins".format(
-        i,
+        total,
         end,
         (end-start).seconds / 60.0))
     
@@ -122,10 +143,22 @@ def process(filepath):
 def populate_entity(entity_class, graph, record):
     entity = rdflib.BNode()
     graph.add((entity, rdflib.RDF.type, entity_class))
-    sparql = GET_ENTITY_MARC.format(entity_class)
-    for prop, marc in MARC2BIBFRAME.query(sparql):
+    sparql = GET_LINKED_CLASSES.format(entity_class)
+    for dest_property, dest_class, prop in MARC2BIBFRAME.query(sparql):
+        #! Should dedup dest_class here, return found URI or BNode
+        bf_class = rdflib.BNode()
+        graph.add((bf_class, rdflib.RDF.type, dest_class))
+        graph.add((entity, prop, bf_class)) 
+        for row in MARC2BIBFRAME.query(
+            GET_MARC.format(dest_class, entity_class)):
+            marc = row[0]
+            pattern = str(marc).split("/")[-1]
+            for value in match_marc(record, pattern):
+                graph.add((bf_class, dest_property, rdflib.Literal(value)))
+    sparql2 = GET_DIRECT_PROPS.format(entity_class)
+    for dest_prop, marc in MARC2BIBFRAME.query(sparql2):
         for value in match_marc(record, str(marc).split("/")[-1]):
-            g.add((work, prop, rdflib.Literal(value)))
+            graph.add((entity, dest_prop, rdflib.Literal(value)))
     return entity
 
 def setup():
@@ -145,10 +178,7 @@ def transform(record):
     """
     # Assumes each MARC record will have at least 1 Work, Instance, and Item
     g = new_graph()
-    work = populate_entity(BF.Work, g, record)
     instance = populate_entity(BF.Instance, g, record)
-    g.add((work, BF.hasInstance, instance))
-    g.add((instance, BF.instanceOf, work))
     item = populate_entity(BF.Item, g, record)
     g.add((instance, BF.hasItem, item))
     g.add((item, BF.itemOf, instance))
