ba90d476d6eabe32e2123968c8130e4c4fd8ebbe
Jeremy Nelson <jermnelson@gmail.com>
2016-09-06 10:10:23 -0600

    adding setup.py for bibcat

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "VERSION")) as version:
+    VERSION = version.read()
+
+setup(
+    name="BIBCAT",
+    version=VERSION,
+    description="BIBCAT RDF Framework Application",
+    author="KnowledgeLinks",
+    author_email="knowledgelinks.io@gmail.com",
+    license="MIT",
+    url="http://knowledgelinks.io/#/products/bibcat",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database :: Database",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing :: Indexing"
+    ],
+    keywords="semantic web bibframe rdf",
+    packages=find_packages(exclude=['tests', 'documentation', 'contrib']),
+    install_requires=[
+        'Flask',
+        'pymarc',
+        'rdfframework',          
+        'requests'
+    ],
+    package_date={
+        "rdf-references": ["*.ttl"],
+        "rdfw-definitions": ["*.ttl"],
+    }
+)      
