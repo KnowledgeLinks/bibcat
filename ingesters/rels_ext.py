6b7375bb011e2de1162596e0afd303ce995431f5
Jeremy Nelson <jermnelson@gmail.com>
2017-02-06 14:35:42 -0700

    added RELS-EXT ingester

---
+++ ingesters/rels_ext.py
@@ -0,0 +1,70 @@
+"""Fedora 3.x RELS-EXTseries to BIBFRAME 2.0 ingester
+
+This ingester is not intended to generated fully formed BF RDF but 
+supplement existing ingesters like MODS and DC. The RELS-EXT ingester adds
+additional properties and classes to existing BF entities. 
+
+"""
+__author__ = "Jeremy Nelson, Mike Stabile"
+
+import rdflib
+import requests
+
+
+from .ingester import Ingester
+
+
+
+class RELSEXTIngester(Ingester):
+
+    def __init__(self, **kwargs):
+        rules = ['kds-bibcat-rels-ext.ttl']
+        if "rules_ttl" in kwargs:
+            tmp_rules = kwargs.get("rules_ttl")
+            if isinstance(tmp_rules, str):
+                rules.append(tmp_rules)
+            elif isinstance(tmp_rules, list):
+                rules.extend(tmp_rules)
+        if 'source' in kwargs:
+            if isinstance(kwargs.get('source'), str):
+                source = rdflib.ConjunctiveGraph()
+                source.parse(data=kwargs.get('source'))
+                kwargs['source'] = source
+        kwargs['rules_ttl'] = rules        
+        super(RELSEXTIngester, self).__init__(**kwargs)
+
+    
+    def __handle_linked_pattern__(self, **kwargs):
+        entity = kwargs.get("entity")
+        destination_class = kwargs.get("destination_class")
+        destination_property = kwargs.get("destination_property")
+        target_property = kwargs.get("target_property")
+        rule = kwargs.get("rule")
+        if isinstance(rule, rdflib.BNode):
+            for pred, obj in self.rules_graph.predicate_objects(subject=rule):
+                if self.source.value(predicate=pred, object=obj) is not None:
+                    print("Adding {} dest_prop={} dest_class={}".format(entity, destination_property, destination_class))
+                    self.graph.add((entity, destination_property, destination_class))
+
+    def transform(self, xml=None, instance_uri=None, item_uri=None):
+        """Overrides parent class transform and adds XML-specific
+        transformations
+
+        Args:
+            xml(str): XML or None
+            instance_uri: URIRef for instance or None
+            item_uri: URIREf for item or None
+        """
+        if xml is not None:
+            if isinstance(xml, str):
+                source = rdflib.ConjunctiveGraph()
+                source.parse(data=xml)
+        super(RELSEXTIngester, self).transform(
+            source=xml,
+            instance_uri=instance_uri,
+            item_uri=item_uri)
+
+                
+
+
+
