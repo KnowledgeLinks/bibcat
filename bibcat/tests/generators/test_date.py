f88fd6fe2507c22ac0707b898f9000d54abd8e13
Jeremy Nelson <jermnelson@gmail.com>
2017-08-08 14:16:44 -0600

    adding date generator

---
+++ bibcat/tests/generators/test_date.py
@@ -0,0 +1,21 @@
+__author__ = "Jeremy Nelson"
+
+import unittest
+
+import bibcat.generators.date
+
+
+class TestDateGenerator(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init_default(self):
+        date_generator = date.DateGenerator()
+        
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    
