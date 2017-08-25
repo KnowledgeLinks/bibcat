fe919c9a7b12dfbda22635ed2a320a655521d3be
Jeremy Nelson <jermnelson@gmail.com>
2017-08-25 14:51:06 -0600

    version bump and added wikidata

---
+++ bibcat/linkers/wikidata.py
@@ -0,0 +1,9 @@
+"""Provides enhancement of existing bibliographic data by consuming info about
+the entity from https://www.wikidata.org/"""
+ 
+__author__ = "Jeremy Nelson"
+
+import requests
+WIKIDATA_SPARQL = "https://query.wikidata.org/sparql"
+
+
