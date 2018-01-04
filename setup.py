1fd6743973f1ea86f3a41b4cab94ffc01aaadfaa
Jeremy Nelson <jermnelson@gmail.com>
2018-01-04 12:16:44 -0700

    Fixing issue https://github.com/KnowledgeLinks/Plains2PeaksPilot/issues/24 for creator, now returning a string

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name="bibcat",
-    version="2.0.2",
+    version="2.0.3",
     description="BIBCAT RDF Framework Application",
     author="KnowledgeLinks",
     author_email="knowledgelinks.io@gmail.com",
